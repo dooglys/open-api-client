cfea2f0d26e20746c24f8e00c60578d18e07a65a
kavi4 <antoniy986@gmail.com>
2019-02-25 21:08:00 +0300

    DGS-4568   Проверить и внести правки в публичное API (#6)
    * DGS-4572 поправил запрос для sales/order/create
    
    * DGS-4572 добавил методы structure/locality/list, structure/locality/view
    
    * DGS-4568 добавил примеры запросов
    
    * DGS-4568 поправил окружение
    
    * DGS-4572 add more methods and improve BadResponseException
    
    * DGS-4572 makes callMethod public

--- src/Exception/BadResponseException.php
+++ src/Exception/BadResponseException.php
@@ -2,10 +2,38 @@
 
 namespace Dooglys\Api\Exception;
 
+use Throwable;
+
 /**
  * Class BadResponseException
  */
 class BadResponseException extends \Exception
 {
+
+    /**
+     * @var
+     */
+    protected $data;
+
+    /**
+     * BadResponseException constructor.
+     * @param string $message
+     * @param int $code
+     * @param Throwable|null $previous
+     * @param $data
+     */
+    public function __construct($message = "", $code = 0, Throwable $previous = null, array $data = [])
+    {
+        $this->data = $data;
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
 
 }
